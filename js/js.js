0e2d0dc7c72b239aa915eb6e1b7a1ded41b13e59
qzx <2735332991@qq.xom>
2024-07-19 19:59:20 +0800

    BOM

--- js/js.js
+++ js/js.js
@@ -1,18 +1,13 @@
-//自定义对象
-var user= {
-  name :"tom",
-  age: 18,
-  eat: function() {
-    alert("吃饭")
-  }
-}
-alert(user.name);
-user.eat();
-json对象
-var jsonstr='{"name":"qzx","age":18,"arr":[1,2,3]}';
-alert(jsonstr)
-//json字符串转换为js对象
-var obj=JSON.parse(jsonstr);
-alert(obj.name);
-//js对象转换为json字符串
-alert(JSON.stringify(obj));
+// window对象
+// window.alert()弹窗
+var flag=confirm("666")//显示带有确定和取消的对话框确定返回true取消返回false
+alert(flag)
+var i=0;
+setInterval(function(){
+  alert(i++)
+},2000)//定时器SetInterval周期性的执行某一个函数
+setTimeout(function () {
+alert(BOM)
+},3000)//定时器1SetTimeout延迟指定时间执行一次
+// location地址栏对象
+alert(location.href);//设置或返回完整的url;给其赋值会立即跳转到指定的url
