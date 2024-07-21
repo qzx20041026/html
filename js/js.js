727ba0f735b6cc94c715318502a807d15d367cc9
qzx <2735332991@qq.xom>
2024-07-21 20:50:38 +0800

    Dom

--- js/js.js
+++ js/js.js
@@ -1,10 +1,6 @@
-//Document对象中提供了以下获取Element元素对象的函数
-//1.根据id属性值获取，返回单个Element对象
-//var h1 = document.getElementById('h1')
-//2.根据标签名称获取，返回Element对象数组
-//var divs = document.getElementsByTagName('div')
-//3.根据name属性值获取，返回Element对象数组
-//var hobbys = document.getElementsByName('hobby')
-//4.根据class属性值获取，返回Element对象数组
-//var clss = document.getElementsByClassName('cls')
-
+function f(){
+  alert("Hello World!");
+}
+function a() {
+  alert("aaa")
+}
