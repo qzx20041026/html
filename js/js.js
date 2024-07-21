b4966769236f738bd008972f789dce9256af2fd9
qzx <2735332991@qq.xom>
2024-07-21 20:18:15 +0800

    Dom

--- js/js.js
+++ js/js.js
@@ -1,13 +1,10 @@
-// window对象
-// window.alert()弹窗
-var flag=confirm("666")//显示带有确定和取消的对话框确定返回true取消返回false
-alert(flag)
-var i=0;
-setInterval(function(){
-  alert(i++)
-},2000)//定时器SetInterval周期性的执行某一个函数
-setTimeout(function () {
-alert(BOM)
-},3000)//定时器1SetTimeout延迟指定时间执行一次
-// location地址栏对象
-alert(location.href);//设置或返回完整的url;给其赋值会立即跳转到指定的url
+//Document对象中提供了以下获取Element元素对象的函数
+//1.根据id属性值获取，返回单个Element对象
+//var h1 = document.getElementById('h1')
+//2.根据标签名称获取，返回Element对象数组
+//var divs = document.getElementsByTagName('div')
+//3.根据name属性值获取，返回Element对象数组
+//var hobbys = document.getElementsByName('hobby')
+//4.根据class属性值获取，返回Element对象数组
+//var clss = document.getElementsByClassName('cls')
+
